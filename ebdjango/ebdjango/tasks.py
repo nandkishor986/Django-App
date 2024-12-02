8473ed8223d2fe1a0b0de6b957ab536a717bf636
nandkishor986 <metangenandkishor2805@gmail.com>
2024-12-02 10:54:39 +0530

    tasks.py modified

--- ebdjango/ebdjango/tasks.py
+++ ebdjango/ebdjango/tasks.py
@@ -3,3 +3,11 @@ from celery import shared_task
 @shared_task
 def add(x, y):
     return x + y
+
+@shared_task
+def multiply(x, y):
+    return x * y
+
+@shared_task
+def greet_user(name):
+    return f"Hello, {name}!"
